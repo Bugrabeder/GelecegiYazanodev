3dc5893cf846e142864bfab2af400b83a5737e43
Buğra Beder <bugrabeder41@outlook.com>
2022-03-23 14:28:40 +0300

    Webödev tamamlandı.

---
+++ WebÖdev/WebÖdev/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebÖdev.Models;
+
+namespace WebÖdev.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Name = "Onur";
+            ViewBag.Age = 27;
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(User user)
+        {
+
+            if (ModelState.IsValid)
+            {
+                return View("Thanks");
+            }
+            
+            return View();
+        }
+    }
+}
---
+++ WebÖdev/WebÖdev/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebÖdev.Models
+{
+    public class User
+    {
+        [Required(ErrorMessage ="Kullanıcı adı boş bırakılamaz.")]
+        public string KullaniciAdi { get; set; }
+        [Phone]
+        public string TelefonNumarasi { get; set; }
+        [Required(ErrorMessage = "Email boş bırakılamaz.")]
+        [EmailAddress(ErrorMessage ="Email formatı hatalı")]
+        public string EMail { get; set; }
+        
+        public string Durum { get; set; }
+
+    }
+}
